d96bd80e7d35bcbd4d09dd4641ec6ff60a7af82f
Ram1453 <ramkumardadi1453@gmail.com>
2025-02-13 18:47:09 +0000

    commit full project

---
+++ Helping_Hands/index.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Database connection
+const db = mysql.createConnection({
+    host: "192.168.0.126",
+    user: "root",
+    password: "Rakesh@2001", 
+    database: "helpinghands"
+});
+
+db.connect(err => {
+    if (err) {
+        console.error("Database connection failed: " + err.stack);
+        return;
+    }
+    console.log("Connected to database");
+});
+
+
+// const server = http.createServer((req, res) => {
+//     if (req.url === "/") {
+//         fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+//             if (err) {
+//                 res.writeHead(500, { "Content-Type": "text/plain" });
+//                 res.end("Internal Server Error");
+//             } else {
+//                 res.writeHead(200, { "Content-Type": "text/html" });
+//                 console.log({data})
+//                 res.end(data);
+//             }
+//         });
+//     } else {
+//         res.writeHead(404, { "Content-Type": "text/plain" });
+//         res.end("Page Not Found");
+//     }
+// });
+
+
+// Get all users
+
+app.get("/users", (req, res) => {
+    db.query("call userList()", (err, results) => {
+        if (err) return res.status(500).json(err);
+        let data = results[0];
+         console.log(data)
+        res.json(data);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    const query = "select id, firstname, lastname, date_format(users.startDate,'%Y-%m-%d') as startDate, totalContribution, lastYearContribution from users where id = ?";
+    
+    db.query(query, [userId], (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error fetching user data");
+      }
+      
+      if (result.length === 0) {
+        return res.status(404).send("User not found");
+      }
+  
+      // Send the user data as response
+      res.json(result[0]);
+    });
+  });
+  
+
+// Add a new user
+app.post("/users", (req, res) => {
+
+    console.log(req.body)
+    const { firstname, lastname, startDate, totalContribution, lastYearContribution } = req.body;
+    const query = "INSERT INTO users (firstname, lastname, startDate, totalContribution, lastYearContribution, createdOn) VALUES (?, ?, ?, ?, ?, NOW())";
+    db.query(query, [firstname, lastname, startDate, totalContribution, lastYearContribution], (err, result) => {
+        if (err) return res.status(500).json(err);
+        res.json({ message: "User added successfully", userId: result.insertId });
+    });
+});
+
+// Update user details
+app.put("/users/:id", (req, res) => {
+    const { firstname, lastname, startDate, totalContribution, lastYearContribution } = req.body;
+    const { id } = req.params;
+    const query = "UPDATE users SET firstname = ?, lastname=?, startDate=?, totalContribution=?, lastYearContribution=?, updatedOn=NOW() WHERE id=?";
+    db.query(query, [firstname, lastname, startDate, totalContribution, lastYearContribution, id], (err, result) => {
+        if (err) return res.status(500).json(err);
+        res.json({ message: "User updated successfully" });
+    });
+});
+
+// Delete a user
+app.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+    db.query("UPDATE users SET isActive=? WHERE id = ?", [0, id], (err, result) => {
+        if (err) return res.status(500).json(err);
+        res.json({ message: "User deleted successfully" });
+    });
+});
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+// Start server
+//const PORT = 3000;
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
